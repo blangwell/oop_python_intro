c117038896971161640b8d29f604cac0c8274de3
blangwell <b.langwell@outlook.com>
2020-09-14 15:30:43 -0700

    [add_blurb] Add add_blurb class method and test

--- video_game.py
+++ video_game.py
@@ -4,7 +4,8 @@ class VideoGame():
     self.esrb = esrb
     self.year = year
     self.score = 0
-  
+    self.blurb = ''
+
   def score_game(self, user_score):
     self.score += user_score
     print('You rated {} a {} out of 10'.format(self.title, self.score))
@@ -15,8 +16,15 @@ class VideoGame():
   def show_year(self):
     print('{} was released in {}'.format(self.title, self.year))
 
+  def add_blurb(self, blurb):
+    self.blurb = blurb
+    print('here is your blurb about :{} \n{}'.format(self.title, blurb))
+  
+
 sekiro = VideoGame('sekiro', 'M', '2019')
 
 sekiro.score_game(9)
 sekiro.show_rating()
 sekiro.show_year()
+
+sekiro.add_blurb('this game is super fun i say!')
