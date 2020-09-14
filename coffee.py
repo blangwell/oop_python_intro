6373c295380b64e8967a8ceb7ce391b66655147c
blangwell <b.langwell@outlook.com>
2020-09-14 14:17:38 -0700

    [oop] Add instances of CoffeeCup class

--- coffee.py
+++ coffee.py
@@ -10,9 +10,29 @@ class CoffeeCup():
   
   def empty(self):
     self.amount = 0
+    print('cup emptied')
 
   def drink(self, amount):
     self.amount -= amount
     if (self.amount == 0):
       self.amount = 0
       print('The cup is currently empty')
+
+# create instances of CoffeeCup class
+adam_cup = CoffeeCup(8)
+rome_cup = CoffeeCup(16)
+pete_cup = CoffeeCup(12)
+taylor_cup = CoffeeCup(2)
+
+rome_cup.fill()
+rome_cup.empty()
+
+adam_cup.fill()
+adam_cup.drink(8)
+print(adam_cup.amount)
+adam_cup.fill()
+print(adam_cup.amount)
+
+pete_cup.fill()
+pete_cup.drink(6)
+print(f'pete has {pete_cup.amount} ounces of coffee left')
